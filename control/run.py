fa871dddeae87e21149fbb56a077ed42568b95ca
serg4life <sergimendi2012@gmail.com>
2025-05-21 19:13:53 +0200

    C related things changed

--- control/run.py
+++ control/run.py
@@ -6,6 +6,8 @@ from subprocess import check_call
 from vunit import VUnit
 
 ROOT = Path(__file__).resolve().parent
+lib_dir = ROOT / "shared"
+lib_dir.mkdir(exist_ok=True)
 
 VU = VUnit.from_argv()
 VU.add_vhdl_builtins()
@@ -17,7 +19,7 @@ LIB.add_source_files([
     ROOT / "test" / "*.vhd",
 ])
 
-check_call(["gcc", "-fPIC", "-shared", str(ROOT / "c" / "caux.c"), "-o", "libcaux.so"])
+check_call(["gcc", "-fPIC", "-shared", str(ROOT / "c" / "cosim_parameters.c"), "-o", str(lib_dir / "libcosim.so")])
 
 if name == 'posix':
     LDPATH = environ.get("LD_LIBRARY_PATH")
